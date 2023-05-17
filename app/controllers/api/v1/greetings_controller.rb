0bc6c35726cb5c3bc433763c03764462a32540d1
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-05-17 18:50:53 -0500

    Update README.md

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::GreetingsController < ApiController
   def index
+    # Another way @greeting = Greeting.all.sample
     @greeting = Greeting.all.order('random()').first
     render json: { greeting: @greeting.message }
   end
